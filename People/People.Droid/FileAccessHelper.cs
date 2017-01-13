431dcd4f3547ed0e16be1108df19c9fe69846095
jerrynixon <Jerry@nixoncorp.com>
2017-01-13 16:34:44 -0700

    Add project files.

---
+++ People/People.Droid/FileAccessHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.Droid
+{
+    public static class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string fileName)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return System.IO.Path.Combine(path, filename);
+        }
+    }
+}
---
+++ People/People.UWP/FileAccessHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.UWP
+{
+    public static class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string fileName)
+        {
+            var folder = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            return System.IO.Path.Combine(folder, fileName);
+        }
+    }
+}
